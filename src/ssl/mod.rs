d4d6b84bb27021f4638e36980d0df03a3c05fe22
Ewen MacCulloch <ewen2076@gmail.com>
2023-12-07 16:28:03 +1100

    Fix SSL

--- src/ssl/mod.rs
+++ src/ssl/mod.rs
@@ -3,17 +3,17 @@ use tokio::sync::Mutex;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 use std::time::Instant;
-use std::fs::read_to_string;
-use crate::components;
+use std::fs::{File, read_to_string};
+use std::io::BufReader;
 use std::path::Path;
 use rustls::{
-    sign::{RSASigningKey, CertifiedKey, SigningKey},
-    internal::pemfile,
-    ServerConfig,
-    NoClientAuth,
+    ServerConfig, NoClientAuth, ResolvesServerCertUsingSNI, 
+    internal::pemfile::{certs, pkcs8_private_keys},
+    sign::{CertifiedKey, RSASigningKey}
 };
 use tokio_rustls::TlsAcceptor;
 use crate::BColors;
+use crate::components;
 
 pub async fn handle_client(configs: Arc<Mutex<Vec<(String, bool, String, String, String)>>>, colors: BColors, client_stream: TcpStream) -> std::io::Result<()> {
     let start_time = Instant::now();
@@ -21,39 +21,40 @@ pub async fn handle_client(configs: Arc<Mutex<Vec<(String, bool, String, String,
     let mut buffer = vec![0; 1024];
     let mut rustls_config = ServerConfig::new(NoClientAuth::new());
 
-    let mut resolver = rustls::ResolvesServerCertUsingSNI::new();
+    let mut resolver = ResolvesServerCertUsingSNI::new();
     for (domain, _, _, ssl_certificate, ssl_certificate_key) in configs.lock().await.iter() {
-        let cert_file = read_to_string(Path::new(ssl_certificate))?;
-        let key_file = read_to_string(Path::new(ssl_certificate_key))?;
-        let mut cert_file_bytes = cert_file.as_bytes();
-        let mut key_file_bytes = key_file.as_bytes();
-        let certs = pemfile::certs(&mut cert_file_bytes);
-        let keys = pemfile::rsa_private_keys(&mut key_file_bytes);
-        
-        match (keys, certs) {
-            (Ok(keys), Ok(certs)) if !keys.is_empty() && !certs.is_empty() => {
-                match RSASigningKey::new(&keys[0]) {
-                    Ok(signing_key) => {
-                        let key: Arc<Box<dyn SigningKey>> = Arc::new(Box::new(signing_key));
-                        resolver.add(domain, CertifiedKey::new(certs.clone(), key)).unwrap();
-            
-                        rustls_config.set_single_cert(certs, keys[0].clone()).map_err(|e| {
-                            eprintln!("{}[ARCTICARCH]{} Failed to create signing key for domain  {}: {:?}", colors.fail, colors.endc, domain, e);
-                            std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to set certificate for domain {}", domain))
-                        })?;
-                    }
-                    Err(_) => {
-                        eprintln!("Failed to create signing key for domain {}", domain);
-                        return Err(std::io::Error::new(
-                            std::io::ErrorKind::Other,
-                            format!("Failed to create signing key for domain {}", domain),
-                        ));
-                    }
-                }
+        let cert_file = &mut BufReader::new(File::open(Path::new(ssl_certificate))?);
+        let key_file = &mut BufReader::new(File::open(Path::new(ssl_certificate_key))?);
+
+        let cert_chain = match certs(cert_file) {
+            Ok(c) if !c.is_empty() => c,
+            _ => {
+                eprintln!("{}[ARCTICARCH]{} Failed to parse certs for domain {}", colors.fail, colors.endc, domain);
+                continue;
             }
-            _ => eprintln!("{}[ARCTICARCH]{} Failed to load certs or keys for domain {}", colors.fail, colors.endc, domain),
+        };
+
+        let keys = match pkcs8_private_keys(key_file) {
+            Ok(k) if !k.is_empty() => k,
+            _ => {
+                eprintln!("{}[ARCTICARCH]{} Failed to parse keys for domain {}", colors.fail, colors.endc, domain);
+                continue;
+            }
+        };
+
+        let key = match RSASigningKey::new(&keys[0]) {
+            Ok(key) => key,
+            Err(_) => {
+                eprintln!("{}[ARCTICARCH]{} Failed to create signing key for domain {}", colors.fail, colors.endc, domain);
+                continue;
+            }
+        };
+        let signing_key = CertifiedKey::new(cert_chain, Arc::new(Box::new(key)));
+
+        if resolver.add(domain, signing_key).is_err() {
+            eprintln!("{}[ARCTICARCH]{} Failed to add certificate for domain {}", colors.fail, colors.endc, domain);
         }
-    }     
+    }
 
     rustls_config.cert_resolver = Arc::new(resolver);
 
@@ -65,19 +66,19 @@ pub async fn handle_client(configs: Arc<Mutex<Vec<(String, bool, String, String,
     let request_path = request_str.split_whitespace().nth(1).unwrap_or("/").to_string();
 
     let domain_and_location = {
-        eprintln!("{}[ARCTICARCH]{} Request path: {:?}", colors.fail, colors.endc, request_str.lines());
+        //eprintln!("{}[ARCTICARCH]{} Request path: {:?}", colors.fail, colors.endc, request_str.lines());
         let host_header = request_str.lines()
         .find(|line| line.starts_with("Host:"))
         .and_then(|line| line.splitn(2, ':').nth(1))
         .and_then(|host| host.split_whitespace().next())
         .map(|host| host.to_string());
 
-        eprintln!("{}[ARCTICARCH]{} Host header: {:?}", colors.fail, colors.endc, host_header);
+        //eprintln!("{}[ARCTICARCH]{} Host header: {:?}", colors.fail, colors.endc, host_header);
         let configs = configs.lock().await;
         let found_domain = configs.iter().find(|(domain, _, _, _, _)| {
             host_header.as_ref() == Some(&domain)
         });
-        eprintln!("{}[ARCTICARCH]{} Found domain: {:?}", colors.fail, colors.endc, found_domain);
+        //eprintln!("{}[ARCTICARCH]{} Found domain: {:?}", colors.fail, colors.endc, found_domain);
         found_domain.cloned()
     };
 
